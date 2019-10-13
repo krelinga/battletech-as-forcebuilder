6a8690ed083543238113fbe1f2cdccbc2160f5bf
Andy Kreling <krelinga@gmail.com>
2019-10-13 16:39:35 -0500

    Add some very skeletal unit tests for the builder.

---
+++ test/net/slothdev/battletech/as/forcebuilder/BuilderTest.kt
@@ -0,0 +1,11 @@
+package net.slothdev.battletech.`as`.forcebuilder
+
+import org.junit.jupiter.api.*
+
+internal class BuilderTest {
+    @Nested inner class Build {
+        @Test fun `test 1`() {
+
+        }
+    }
+}
