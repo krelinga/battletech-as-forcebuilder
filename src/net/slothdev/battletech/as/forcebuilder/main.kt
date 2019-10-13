db51a281e2f57e2cf74b48feacfa1f6a0ffebb41
Andy Kreling <krelinga@gmail.com>
2019-10-13 16:17:04 -0500

    Add a few more miniatures to main, just playing around.

--- src/net/slothdev/battletech/as/forcebuilder/main.kt
+++ src/net/slothdev/battletech/as/forcebuilder/main.kt
@@ -1,7 +1,9 @@
 package net.slothdev.battletech.`as`.forcebuilder
 
 fun main() {
-    val minis = listOf<Miniature>(Miniature("foo"), Miniature("bar"))
+    val minis = listOf<Miniature>(
+        Miniature("foo"), Miniature("bar"), Miniature("biff"), Miniature("bonk")
+    )
     val builder = Builder(minis)
-    println(builder.build(1))
+    println(builder.build(2))
 }
