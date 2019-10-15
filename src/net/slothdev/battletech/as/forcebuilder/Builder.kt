bf78b69f47bcdd83db5ab2fe75d57247ab019421
Andy Kreling <krelinga@gmail.com>
2019-10-14 21:47:46 -0500

    Really change forces to be side-agnostic.

--- src/net/slothdev/battletech/as/forcebuilder/Builder.kt
+++ src/net/slothdev/battletech/as/forcebuilder/Builder.kt
@@ -3,8 +3,52 @@ package net.slothdev.battletech.`as`.forcebuilder
 import kotlin.math.abs
 import kotlin.math.sqrt
 
-data class Forces(val side1: List<Miniature>, val side2: List<Miniature>)
+data class Forces(val targetPv: Int, private val miniatures: Set<Set<Miniature>>) {
+    init {
+        require(targetPv > 0) { "targetPv must be > 0, saw $targetPv"}
+        require(miniatures.size == 2) { "expected 2 entries in miniatures, saw ${miniatures.size}" }
+    }
 
+    // Allow specifying the sets for side1 and side2 gracefully.
+    constructor(targetPv: Int, side1: Set<Miniature>, side2: Set<Miniature>) : this(
+            targetPv, setOf(side1, side2))
+
+    // Some convenience methods for dealing with the expected number of sides.
+    val side1
+        get() = miniatures.first()
+    val side2
+        get() = miniatures.last()
+
+    val score: Double
+        get() {
+            val side1Points = side1.fold(0) { sum, element -> sum + element.pv }
+            val side2Points = side2.fold(0) { sum, element -> sum + element.pv }
+
+            fun pointDiff(x: Int, y: Int): Double {
+                val diff = abs(x.toDouble() - y.toDouble())
+                return 1.0 / (1.0 + diff)
+            }
+
+            // Terms between 0 and 1 that get smaller as the difference between sideNPoints and targetPv increases.
+            val side1TotalTerm = pointDiff(targetPv, side1Points)
+            val side2TotalTerm = pointDiff(targetPv, side2Points)
+
+            // Term between 0 and 1 that gets smaller as the difference between side1Points and Side2Points increases.
+            val pointDifferenceTerm = pointDiff(side1Points, side2Points)
+
+            // Take all the terms into account.  Treat the per-side total terms as less important than the difference
+            // between the two sides.
+            return sqrt(side1TotalTerm) * sqrt(side2TotalTerm) * pointDifferenceTerm
+        }
+}
+
+class BuilderException(message: String) : Exception(message)
+
+// TODO: there's an optimization opportunity here right now.  I decided that side1 and side2 are
+//  interchangeable, but the way that we generate possible forces doesn't account for that right
+//  now, so we end up making the same force but with side1 and side2 swapped.  That means we're
+//  doing roughly 2x the work that is needed.  This implementation should still give correct
+//  results, it will just be inefficient.
 class Builder private constructor(private val minis: List<Miniature>) {
     // We want to enforce that the given set of minis is unique, but internally it makes more sense
     // to store them as a list.
