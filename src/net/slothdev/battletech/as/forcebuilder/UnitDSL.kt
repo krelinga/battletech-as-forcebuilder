7a3cd28a296f7ada1a8a40b56000663ce70d15c1
Andy Kreling <krelinga@gmail.com>
2019-10-20 12:07:56 -0500

    very basic DSL for alpha strike units.
    Lots of work left to do:
    - properties to add
    - the class structure of the DSL support code could use work.
    - more cases to test.

---
+++ src/net/slothdev/battletech/as/forcebuilder/UnitDSL.kt
@@ -0,0 +1,50 @@
+package net.slothdev.battletech.`as`.forcebuilder
+
+interface UnitDslProperties {
+    var mv: Int?
+    var mvj: Int?
+}
+
+data class UnitDslContext(override var mv: Int? = null, override var mvj: Int? = null) :
+        UnitDslProperties
+
+fun propertiesToUnit(family: String, generation: String, variant: String, pv: Int,
+                     properties: UnitDslProperties): Unit {
+    val nameParts = mutableListOf<String>()
+    if (family.isNotEmpty()) {
+        nameParts.add(family)
+    }
+    if (generation.isNotEmpty()) {
+        nameParts.add(generation)
+    }
+    if (variant.isNotEmpty()) {
+        nameParts.add(variant)
+    }
+    return Unit(nameParts.joinToString(" "), pv)
+}
+
+open class UnitDslGenerationContext(protected val family: String, protected val generation: String,
+                                    protected val baseContext: UnitDslContext,
+                                    protected val unitDb: UnitDb) :
+        UnitDslProperties by baseContext {
+    fun variant(name: String, pv: Int, builder: UnitDslContext.() -> kotlin.Unit) {
+        val context = baseContext.copy()
+        context.builder()
+
+        unitDb.add(propertiesToUnit(family, generation, name, pv, context))
+    }
+
+    fun variant(pv: Int, builder: UnitDslContext.() -> kotlin.Unit) {
+        variant("", pv, builder)
+    }
+}
+
+class UnitDslFamilyContext(family: String, unitDb: UnitDb,
+                           context: UnitDslContext = UnitDslContext()) :
+        UnitDslGenerationContext(family, "", context, unitDb) {
+    fun generation(name: String, builder: UnitDslGenerationContext.() -> kotlin.Unit) {
+        val context = baseContext.copy()
+        val generationContext = UnitDslGenerationContext(family, name, context, unitDb)
+        generationContext.builder()
+    }
+}
---
+++ test/net/slothdev/battletech/as/forcebuilder/UnitDSLTest.kt
@@ -0,0 +1,63 @@
+package net.slothdev.battletech.`as`.forcebuilder
+
+import org.junit.jupiter.api.Test
+import kotlin.test.*
+
+internal class UnitDSLTest {
+    @Test
+    fun `variant in family`() {
+        val unitDb = UnitDb()
+        val familyContext = UnitDslFamilyContext("f", unitDb)
+        familyContext.apply {
+            mv = 10
+
+            variant("foo", 10) {
+                mvj = 8
+            }
+        }
+        assertEquals(unitDb.asSet(), setOf(Unit("f foo", 10)))
+    }
+
+    @Test
+    fun `variant in generation in family`() {
+        val unitDb = UnitDb()
+        val familyContext = UnitDslFamilyContext("f", unitDb)
+        familyContext.apply {
+            mv = 10
+
+            generation("bar") {
+                variant("foo", 20) {
+                    mvj = 8
+                }
+            }
+        }
+        assertEquals(unitDb.asSet(), setOf(Unit("f bar foo", 20)))
+    }
+
+    @Test
+    fun `multiple variants in generation`() {
+        val unitDb = UnitDb()
+        val familyContext = UnitDslFamilyContext("f", unitDb)
+        familyContext.apply {
+            mv = 10
+
+            generation("bar") {
+                variant("foo", 30) {
+                    mvj = 8
+                }
+                variant("foo2", 40) {
+                    mvj = 8
+                }
+                variant("foo3", 50) {
+                    mvj = 8
+                }
+                variant(60) {
+                    mvj = 6
+                }
+            }
+        }
+        assertEquals(unitDb.asSet(),
+                     setOf(Unit("f bar foo", 30), Unit("f bar foo2", 40), Unit("f bar foo3", 50),
+                           Unit("f bar", 60)))
+    }
+}
