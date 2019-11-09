f0a953cec4a876e39a36610e4f76bd0eb9065d14
Andy Kreling <krelinga@gmail.com>
2019-11-09 13:47:50 -0600

    Revert "New Score interface which allows more in the way of children."
    This reverts commit 6daac50a

--- src/net/slothdev/battletech/as/forcebuilder/Scorer.kt
+++ src/net/slothdev/battletech/as/forcebuilder/Scorer.kt
@@ -2,15 +2,6 @@ package net.slothdev.battletech.`as`.forcebuilder
 
 data class OldScore(val finalScore: Double, val components: List<Pair<String, Double>>)
 
-// Interfaces for general scoring.
-interface Score {
-    // The actual numerical score.
-    val score: Double
-
-    // An explanation of how the numerical score was arrived at.
-    fun explain(): String
-}
-
 interface Scorer {
     operator fun invoke(side1: Set<Miniature>, side2: Set<Miniature>): OldScore
 }
